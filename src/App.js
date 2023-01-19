a4cec1c6f7310bbd7fb313c0cb4c35ede289a317
ste2461 <putellistefano@gmail.com>
2023-01-19 14:25:17 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+import felice from "./img/felice.png";
+import triste from "./img/triste.png";
+import sole from "./img/sole.jpg";
+
+import Temp from "./comp/temp";
+import SimpleDialog from "./comp/slider";
+
+function App() {
+
+  const boxStyle = "m-auto flex justify-center align-middle relative "
+
+  const [mood, setMood] = useState(1);
+  const [viewXY, setViewXY] = useState(window.innerWidth > window.innerHeight);
+  const [open, setOpen] = useState(false);
+  const [light, setLight] = useState(0);
+
+  const handleClose = (value) => {
+    console.log(value)
+    setLight(value);
+    setOpen(false);
+  };
+
+  window.addEventListener("resize", () => {
+    setViewXY(getViewXY());
+  });
+
+  function getViewXY() {
+    return (window.innerWidth > window.innerHeight)
+  }
+
+  function sunClicked() {
+    setOpen(true);
+  }
+
+  function getSunSize(size_) {
+    return {
+      clipPath: "circle(" + shrinkSun(light) + "% at 50% 50%)"
+    }
+  }
+
+  function shrinkSun(size_) {
+    if (size_ > 100) {
+      return 100;
+    } else {
+      return (size_ / 5 + 30);
+    }
+  }
+
+  //add gauge display 
+
+  return (
+    <div className="App">
+      <SimpleDialog
+        light={light}
+        open={open}
+        onClose={handleClose}
+      />
+      <div className="h-screen w-screen flex justify-center align-middle">
+        <div className={viewXY ? boxStyle + "w-[100vh] h-[100vh]" : boxStyle + "w-[100vw] h-[100vw]"}>
+          <img id="sole" className="absolute top-0 left-0 w-[30%] mt-10 ml-2 cursor-pointer" style={getSunSize()} onClick={sunClicked} src={sole} alt="sole" />
+          <div className="mt-[50%] w-[30%]">
+            <img src={mood ? felice : triste} alt="felice" onClick={() => {
+              setMood(!mood);
+            }} />
+          </div>
+        </div>
+      </div>
+      <Temp temp="20" />
+      <Temp temp="20" />
+      <Temp temp="20" />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      primary: "#fad5c8",
+      secondary: "#f8f8f8"
+    },
+    plugins: [
+      require('tailwind-clip-path'),
+    ]
+  }
+}
