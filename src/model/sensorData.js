99f5a85427a8e01e28b22aa724b0b076ebe3eaa1
imrik <imri.koch@mail.huji.ac.il>
2023-01-25 17:33:00 +0100

    fix soil moisture

--- src/model/sensorData.js
+++ src/model/sensorData.js
@@ -1,10 +1,9 @@
 class SensorData {
   constructor(data) {
-    // console.log(data);
     this.date = new Date(data.date * 1000);
     this.temperature = data.temperature;
     this.humidity = data.humidity;
-    this.soilMoisture = data.soilMoisture;
+    this.soilMoisture = data.soilMoisture ? data.soilMoisture : 0;
     this.lightPower = data.lightPower;
     this.irrigation = data.irrigation === 1 ? true : false;
     this.waterTank = data.waterTank;
